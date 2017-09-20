aba2693d473bf760e6863e5afa60243b71675a39
Bryan Endermuhle <bendermuhle@my.stlcc.edu>
2017-09-19 22:02:37 -0500

    added caesar.py

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, request
+from caeser import rotate_string
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form="""
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px;
+                width: 540px;
+                height: 120px:
+            }
+        </style>
+    </head>
+    <body>
+         <form  method="post">
+            <label for="rot">Rotate by:  </label >
+            <input type="text" name="rot" placeholder="0"  />
+            
+            <textarea name="textarea">
+            </textarea>
+            
+              <input type="submit" />
+        </form>
+    </body>
+</html>
+"""
+@app.route("/")
+def index():
+   return form
+
+
+app.run()
